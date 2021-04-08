8b2d064783017386d1a0730afbe3468cc7fff97d
zhuangpeng <zhuangpeng@asiainfo.com>
2021-04-08 14:13:55 +0800

    枚举式单例

--- design-demo/src/main/java/com/zpwtt/singleton/Singleton07.java
+++ design-demo/src/main/java/com/zpwtt/singleton/Singleton07.java
@@ -3,10 +3,14 @@ package com.zpwtt.singleton;
 /**
  * 枚举
  * 不仅可以解决线程同步，还可以防止反序列化
+ * 枚举类是没有构造方法的，所不能被java反射去实例化
  * @author zhuangpeng
  */
 public enum Singleton07 {
 
+    /**
+     * 单例对象
+     */
     INSTANCE;
 
     public void say() {
